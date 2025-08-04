dec736879579596c5ce30a85269d00d7d2b12e24
André Lago <andrelago.eu@gmail.com>
2025-08-04 21:49:20 +0100

    Minor fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -208,7 +208,7 @@ export default function BlogPostPage({ params }: { params: { slug: string } }) {
       <nav className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
         <div className="container flex h-14 items-center">
           <div className="mr-4 flex">
-            <Link href="/" className="mr-6 flex items-center space-x-2">
+            <Link href="/" className="mr-6 flex items-center space-x-2 pl-6">
               <span className="font-bold">André Lago</span>
             </Link>
           </div>
